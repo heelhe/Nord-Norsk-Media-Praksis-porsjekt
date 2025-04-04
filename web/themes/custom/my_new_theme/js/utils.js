0f35d6762c423f5545b2184b014805425ff40db1
heelhe <119928888+heelhe@users.noreply.github.com>
2025-04-04 14:32:25 +0200

    removed unused theme

--- web/themes/custom/my_new_theme/js/utils.js
+++
@@ -1,9 +0,0 @@
-module.exports.makeBlue = ($element) => {
-  $element.removeClass('red');
-  $element.addClass('blue');
-};
-
-module.exports.makeRed = ($element) => {
-  $element.removeClass('blue');
-  $element.addClass('red');
-};
